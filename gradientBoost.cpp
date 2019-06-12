9e88d2f2f06cae514b09aeb362a30e1e61ff3d55
pr33ch <will.yang1247@gmail.com>
2019-06-11 18:59:12 -0700

    parsed the csv into a data table format that is more easily usable for our purposes

--- gradientBoost.cpp
+++ gradientBoost.cpp
@@ -2,13 +2,23 @@
 #include <string>
 #include "CSVRow.h"
 
+#define N_VARIABLES 4
+
 int main()
 {
-    std::ifstream       file("plop.csv");
+	std::string filename = std::to_string(N_VARIABLES) + "d.txt";
+    std::ifstream       file(filename);
 
-    CSVRow              row;
-    while(file >> row)
+    CSVRow				variable;
+
+    // data_table[i][j] corresponds to the ith data point and jth variable. If j = N_VARIABLES, j
+    // is the output of the ith data point
+    CSVRow              data_table[N_VARIABLES + 1];
+    int row = 0;
+    while(file >> variable)
     {
-        std::cout << "4th Element(" << row[3] << ")\n";
+        data_table[row] = variable;
+    	std::cout << "4th Element(" << data_table[row][3] << ")\n";
+        row++;
     }
 }	
