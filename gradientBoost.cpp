0174bb8f79fd418aaa3d240bc11779af3e426749
Eric Dang <edang830@gmail.com>
2019-06-11 19:10:46 -0700

    introduce main vars and memory copying

--- gradientBoost.cpp
+++ gradientBoost.cpp
@@ -3,6 +3,7 @@
 #include "CSVRow.h"
 
 #define N_VARIABLES 4
+#define N_DATA 100000
 
 int main()
 {
@@ -21,4 +22,76 @@ int main()
     	std::cout << "4th Element(" << data_table[row][3] << ")\n";
         row++;
     }
-}	
+
+    // initialize data
+    float actual[N_DATA] __attribute__((aligned(64)));
+    float predicted[N_DATA] __attribute__((aligned(64)));
+
+    preprocessing(actual, predicted);
+
+    // Allocate memory
+    cudaError_t err = cudaSuccess;
+    size_t size_input = N_DATA * N_VARIABLES * sizeof(float);
+    size_t size_output = N_DATA * sizeof(float);
+
+    float *d_leafAssignment;
+    float *d_actual;
+    float *d_predicted;
+    float *d_residual;
+    float *d_leafValue;
+
+    // allocate d_leafAssignment memory
+    err = cudaMalloc((void **)&d_leafAssignment, size_output);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device vector d_leafAssignment (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    // allocate d_actual memory
+    err = cudaMalloc((void **)&d_actual, size_output);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device vector d_actual (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    // allocate d_predicted memory
+    err = cudaMalloc((void **)&d_predicted, size_output);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device vector d_predicted (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    // allocate d_residual memory
+    err = cudaMalloc((void **)&d_residual, size_output);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device vector d_residual (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    // allocate d_leafValue memory
+    err = cudaMalloc((void **)&d_leafValue, size_output);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device vector d_leafValue (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    // transfer memory associated with initalized vars from host to device
+    err = cudaMemcpy(d_actual, actual, size_output, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy vector from host to device (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMemcpy(d_predicted, predicted, size_output, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy vector from host to device (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+}
