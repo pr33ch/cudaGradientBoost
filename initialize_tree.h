1ac943a31411ff0bce38bb9872a6bdf8754674d3
pr33ch <will.yang1247@gmail.com>
2019-06-12 20:31:24 -0700

    missing semicolon

--- initialize_tree.h
+++ initialize_tree.h
@@ -4,7 +4,7 @@ void initialize_tree(CSVRow data_table[], float * tree)
 {
 	for (int nth_variable = 0; nth_variable < data_table[0].size()-1; nth_variable++)
 	{
-		float average = 0
+		float average = 0;
 		for (int nth_sample = 0; nth_sample < data_table.size(); nth_sample ++)
 		{
 			average += data_table[nth_sample][nth_variable]/data_table.size();
